74723f080d0cce226f0786cc6e350b2804cdee8a
nathan-lang <76454416+nathan-lang@users.noreply.github.com>
2024-11-17 14:28:42 -0500

    Run app on port 8000.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const PORT = 3000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log("Listening...");
 });
